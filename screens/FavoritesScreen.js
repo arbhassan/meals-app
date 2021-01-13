14cdc5b8250ec433c03dba8a0a87f870f9794af3
arbhassan <hassanjawad1321@gmail.com>
2021-01-13 15:07:26 +0500

    Added imports to FavoritesScreen

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -2,6 +2,9 @@ import React from "react";
 import MealList from "../components/MealList";
 import { MEALS } from "../data/dummy-data";
 
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+import HeaderButton from "../components/HeaderButton";
+
 const FavoritesScreen = (props) => {
   const favMeals = MEALS.filter((meal) => meal.id === "m1" || meal.id === "m2");
   return <MealList listData={favMeals} navigation={props.navigation} />;
